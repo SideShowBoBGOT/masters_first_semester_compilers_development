4ba65442b5b6a9cc4c9e8897c763079f06c1da78
account <account@mail.com>
2025-10-23 14:43:59 +0300

    add token table

--- lab_3/main.c
+++ lab_3/main.c
@@ -1,5 +1,6 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#include <stdint.h>
 #endif
 #include <inttypes.h>
 #include <stdio.h>
@@ -68,8 +69,21 @@ typedef enum {
     TOKEN_TYPE_INTEGER,
     TOKEN_TYPE_FLOAT,
     TOKEN_TYPE_STRING,
-    TOKEN_TYPE_COUNT,
+    TOKEN_TYPE_NEWLINE,
+    TOKEN_TYPE_WHITESPACE,
 } TokenType;
+
+static const char* token_regex_map[] = {
+    [TOKEN_TYPE_IDENTIFIER] = "[a-zA-Z!$%&*/:<=>?^_~][a-zA-Z!$%&*/:<=>?^_~0-9]*|[+]|[-]",
+    [TOKEN_TYPE_BOOLEAN] = "true|false",
+    [TOKEN_TYPE_LPAREN] = "[(]",
+    [TOKEN_TYPE_RPAREN] = "[)]",
+    [TOKEN_TYPE_INTEGER] = "[+-]?[0-9]+",
+    [TOKEN_TYPE_FLOAT] = "[+-]?[0-9]+[.][0-9]+",
+    [TOKEN_TYPE_STRING] = "\"[^\"]*\"",
+    [TOKEN_TYPE_NEWLINE] = "\n",
+    [TOKEN_TYPE_WHITESPACE] = "[ \t]+",
+};
 
 static const char* token_type_str(const TokenType type) {
     #define DEFINE_CASE(val) case val: return STRINGIFY(val)
@@ -81,7 +95,8 @@ static const char* token_type_str(const TokenType type) {
             DEFINE_CASE(TOKEN_TYPE_INTEGER);
             DEFINE_CASE(TOKEN_TYPE_FLOAT);
             DEFINE_CASE(TOKEN_TYPE_STRING);
-            DEFINE_CASE(TOKEN_TYPE_COUNT);
+            DEFINE_CASE(TOKEN_TYPE_NEWLINE);
+            DEFINE_CASE(TOKEN_TYPE_WHITESPACE);
             default: {
                 ASSERT(false);
             }
@@ -97,43 +112,56 @@ typedef struct {
 
 static void analyze_lexic(const char *const str) {
     regex_t regex;
-    static const char regstring[] = "^((\n)|[ \t]+|([a-zA-Z!$%&*/:<=>?^_~][a-zA-Z!$%&*/:<=>?^_~0-9]*|[+]|[-])|(true|false)|([(])|([)])|([+-]?[0-9]+)|([+-]?[0-9]+[.][0-9]+)|(\"[^\"]*\"))";
-    LOG_DEBUG("%s", regstring);
-    ASSERT(regcomp(&regex, regstring, REG_EXTENDED) == 0);
+    {
+        size_t regex_string_len = 0;
+        regex_string_len += ARRAY_COUNT(token_regex_map) - 1;
+        regex_string_len += ARRAY_COUNT(token_regex_map) * 2;
+        for(uint8_t i = 0; i < ARRAY_COUNT(token_regex_map); ++i) {
+            regex_string_len += strlen(token_regex_map[i]);
+        }
+        regex_string_len += 3;
+        char *const regex_string = malloc(regex_string_len + 1);
+        regex_string[0] = '\0';
+        strcat(regex_string, "^(");
+            strcat(regex_string, "(");
+                strcat(regex_string, token_regex_map[0]);
+            strcat(regex_string, ")");
+            for(uint8_t i = 1; i < ARRAY_COUNT(token_regex_map); ++i) {
+                strcat(regex_string, "|");
+                strcat(regex_string, "(");
+                    strcat(regex_string, token_regex_map[i]);
+                strcat(regex_string, ")");
+            }
+        strcat(regex_string, ")");
+        LOG_DEBUG("%s", regex_string);
+        ASSERT(regcomp(&regex, regex_string, REG_EXTENDED) == 0);
+        free(regex_string);
+    }
     regoff_t offset = 0;
-    enum {
-        group_offset =
-            + 1 // for global match
-            + 1 // for outer group
-            + 1 // for newline group
-    };
-    regmatch_t regmatch[(int)TOKEN_TYPE_COUNT + (int)group_offset] = {0};
+    regmatch_t regmatch[ARRAY_COUNT(token_regex_map) + 1] = {0};
     regoff_t newline_index = 0;
     regoff_t newline_offset = 0;
+    static const uint8_t group_offset = 2;
     while(regexec(&regex, str + offset, ARRAY_COUNT(regmatch), regmatch, 0) != REG_NOMATCH) {
         // for debug puposes i want to be sure that token matches only for a single group
         bool group_found = false;
-        if(regmatch[2].rm_so != -1 && regmatch[2].rm_eo != -1) {
-            ASSERT(group_found == false);
-            group_found = true;
-            // const int token_len = regmatch[2].rm_eo - regmatch[2].rm_so;
-            // printf("NEWLINE %.*s", token_len, str + offset + regmatch[2].rm_so);
-            newline_index += 1;
-            newline_offset = offset + regmatch[0].rm_eo;
-        }
-        for(int8_t i = group_offset; i < (int8_t)ARRAY_COUNT(regmatch); ++i) {
+        for(uint8_t i = group_offset; i < (uint8_t)ARRAY_COUNT(regmatch); ++i) {
             if(regmatch[i].rm_so != -1 && regmatch[i].rm_eo != -1) {
+                const TokenType token_type = (TokenType)i - group_offset;
+                if(token_type == TOKEN_TYPE_NEWLINE) {
+                    newline_index += 1;
+                    newline_offset = offset;
+                }
                 ASSERT(group_found == false);
                 group_found = true;
                 const int token_len = regmatch[i].rm_eo - regmatch[i].rm_so;
-                LOG_DEBUG("%.*s, %s", token_len, str + offset + regmatch[i].rm_so, token_type_str((TokenType)i - (int)group_offset));
+                LOG_DEBUG("%.*s, %s", token_len, str + offset + regmatch[i].rm_so, token_type_str(token_type));
             }
         }
         offset += regmatch[0].rm_eo;
     }
     if(offset != (regoff_t)strlen(str)) {
         LOG_DEBUG("unrecognized input at %d:%d", newline_index, offset - newline_offset);
-        // printf("invalid input: %s", )
     }
 }
 
